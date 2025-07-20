f6d6f73d2b40f253759fcb65bb6ec7fb056ba103
koteswar <koteswarp@skyflow.com>
2025-07-20 15:41:43 +0530

    ansible terraform modules

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -35,8 +35,8 @@ mkdir -p /var/lib/jenkins/.ssh
 
 cat > /var/lib/jenkins/.aws/credentials <<EOF
 [default]
-aws_access_key_id = ***
-aws_secret_access_key = ***
+aws_access_key_id = *****
+aws_secret_access_key = *****
 EOF
 
 chown -R jenkins:jenkins /var/lib/jenkins/.aws
